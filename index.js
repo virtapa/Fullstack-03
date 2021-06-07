73481f16b6d0124ef3833be1bcd23fc2f48f932d
Panu Virta <panu.virta@student.hamk.fi>
2021-06-07 10:15:59 +0300

    first

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require('cors')
+
+app.use(cors())
+
 
 app.use(express.json());
 
@@ -106,7 +110,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
